098a8deae0d2ccd9a10f3afeef366cdabd002dcb
Janice-M <janiceink001@gmail.com>
2019-06-16 22:41:18 +0300

    html

---
+++ js/index.js
@@ -0,0 +1,42 @@
+console.log('redbull');
+console.log('bruh');
+console.log(69);
+
+$(document).ready(function () {
+
+    $(".whatwedo p").hide();
+
+    $('.whatwedo img.one').click(function () {
+        $(".whatwedo img.one").toggle();
+        $(".whatwedo p.one").toggle();
+    });
+    $('.whatwedo p.one').click(function () {
+        $(".whatwedo img.one").toggle();
+        $(".whatwedo p.one").toggle();
+    });
+    $('.whatwedo img.two').click(function () {
+        $(".whatwedo img.two").toggle();
+        $(".whatwedo p.two").toggle();
+    });
+    $('.whatwedo p.two').click(function () {
+        $(".whatwedo img.two").toggle();
+        $(".whatwedo p.two").toggle();
+    });
+    $('.whatwedo img.three').click(function () {
+        $(".whatwedo img.three").toggle();
+        $(".whatwedo p.three").toggle();
+    });
+    $('.whatwedo p.three').click(function () {
+        $(".whatwedo img.three").toggle();
+        $(".whatwedo p.three").toggle();
+    });
+
+    $('.whatwedo h3').click(function () {
+        $(".whatwedo img.one").toggle();
+        $(".whatwedo p.one").toggle();
+    });
+    $('.whatwedo h3').click(function () {
+        $(".whatwedo img.one").toggle();
+        $(".whatwedo p.one").toggle();
+    });
+});
